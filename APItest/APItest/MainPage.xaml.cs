9ae6a2b81fd5360c5f67c675900150e1fa7db721
Aswin P G <aswinpg@outlook.in>
2019-08-13 13:45:42 +0530

    refreshed

--- APItest/APItest/MainPage.xaml.cs
+++ APItest/APItest/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace APItest
 {
@@ -18,6 +19,8 @@ namespace APItest
 
 
         Data data;
+        Datas dat;
+        string v = "gjhvjh";
         Data data1;
         int i = 0;
         Data data2;
@@ -25,54 +28,136 @@ namespace APItest
         {
             InitializeComponent();
 
+
             
-            data1 = new Data()
-            {
-                message = "Test",
-                strings = new List<string>
-                {
-                    "Aswin","Haris","Arshu"
-                }
-            };
 
             LoadContent();
         }
 
-        public async Task LoadContent()
+        public async void LoadContent()
         {
 
-            
 
-            string url = "https://us-central1-tuto-ff870.cloudfunctions.net/helloWorld";
+            //Datas datasList = new Datas()
+            //{
+            //    datas =
+            //    {
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        },
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        },
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        }
+            //    }
+            //};
+
+            //Datas datasList2 = new Datas()
+            //{
+            //    datas =
+            //    {
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        },
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        },
+            //        new Data()
+            //        {
+            //            Description="njknjk",
+            //            Heading="jkbjk",
+            //            Name="njnjk",
+            //            PostedOn="njkn",
+            //            Status="njnjk"
+            //        }
+            //    }
+            //};
+
+            //var X = datasList.datas[0];
+
+
+            //datasList.datas.Add(datasList2.datas[0]);
+
+
+            string url = "https://us-central1-tuto-ff870.cloudfunctions.net/ForumDisplay1";
 
             HttpResponseMessage responseMessage = null;
+            HttpResponseMessage responseMessage1 = null;
+
+            //HttpContent httpContent = new FormUrlEncodedContent(data);
+
+            Req req = new Req()
+            {
+                Status = "Discussion",
+                PostedOn = "first"
+            };
+
+            //HttpContent httpContent = new FormUrlEncodedContent(req);
+
+            var json = JsonConvert.SerializeObject(req);
 
             try
             {
-                responseMessage = await APIHelper.APIClient.GetAsync(url);
+                //responseMessage = await APIHelper.APIClient.GetAsync(url);
+                responseMessage1 = await APIHelper.APIClient.PostAsJsonAsync(url, req);
             }
             catch(Exception e)
             {
                 Console.WriteLine(e);
             }
             //HttpResponseMessage responseMessage = await APIHelper.APIClient.GetAsync(url);
-            HttpResponseMessage responseMessage1 = await APIHelper.APIClient.PostAsJsonAsync(url, data1);
 
-            var content = responseMessage.Content.ReadAsStringAsync();
 
-            data = await responseMessage.Content.ReadAsAsync<Data>();
-            data2 = await responseMessage1.Content.ReadAsAsync<Data>();
+            //var content = responseMessage.Content.ReadAsStringAsync();
 
+
+
+            //data = await responseMessage.Content.ReadAsAsync<Data>();
+            //data2 = await responseMessage1.Content.ReadAsAsync<Data>();
+            var x = await responseMessage1.Content.ReadAsStringAsync();
+            dat = await responseMessage1.Content.ReadAsAsync<Datas>();
+
+            Datas datas67 = new Datas();
+
+            var testvar = await responseMessage1.Content.ReadAsStringAsync();
+            Newtonsoft.Json.JsonConvert.PopulateObject(testvar, datas67);
             
         }
 
 
 
-        public void Addcontent()
-        {
-            i++;
-            data1 = new Data() { message = "Added" + i.ToString(), strings = new List<string> { "Aswin" + i.ToString(), "Haris" + i.ToString(), "Arshu" + i.ToString() } };
-        }
+        //public void Addcontent()
+        //{
+        //    i++;
+        //    data1 = new Data() { message = "Added" + i.ToString(), strings = new List<string> { "Aswin" + i.ToString(), "Haris" + i.ToString(), "Arshu" + i.ToString() } };
+        //}
 
 
 
@@ -81,17 +166,40 @@ namespace APItest
         private async void Button_Clicked(object sender, EventArgs e)
         {
 
-            Addcontent();
-            await LoadContent();
-            Message.Text = data2.message;
-            st1.Text = data2.strings[0];
-            st2.Text = data2.strings[1];
-            st3.Text = data2.strings[2];
+            //Addcontent();
+            //await LoadContent();
+            //Message.Text = data2.message;
+            //st1.Text = data2.strings[0];
+            //st2.Text = data2.strings[1];
+            //st3.Text = data2.strings[2];
         }
     }
     public class Data
     {
-        public string message { get; set; }
-        public List<string> strings { get; set; }
+        public string Description { get; set; }
+        public string Heading { get; set; }
+        public string Name { get; set; }
+        public string PostedOn { get; set; }
+        public string Status { get; set; }
+
     }
+
+
+    public class Questions
+    {
+        public string docID { get; set; }
+        public Data q { get; set; }
+    }
+
+
+    public class Datas
+    {
+        public List<Questions> Questions { get; set; }
+    }
+    public class Req
+    {
+        public string Status { get; set; }
+        public string PostedOn { get; set; }
+    }
+    
 }
